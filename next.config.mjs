f47b75b1a9691ccffa7130a0ab6aea67d4a80fbc
Nicolas <nicolasalexandre0001@gmail.com>
2024-06-10 21:03:52 -0300

    config: update react

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
+  reactStrictMode: true,
   images: {
     dangerouslyAllowSVG: true, // remove it
     remotePatterns: [
