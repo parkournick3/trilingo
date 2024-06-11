949ec50868c0c7984775bb7bc7f2e8b32d8d97cc
Nicolas <nicolasalexandre0001@gmail.com>
2024-06-10 21:35:43 -0300

    style: sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,9 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import SidebarItem from "./sidebar-item";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 type Props = {
   className?: string;
@@ -12,7 +17,59 @@ export default function Sidebar({ className }: Props) {
         className
       )}
     >
-      Sidebar
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image
+            src="https://placehold.co/40x40"
+            height={40}
+            width={40}
+            alt="Mascot"
+          />
+          <h1 className="text-2xl font-extrabold tracking-wide text-green-600">
+            Trilingo
+          </h1>
+        </div>
+      </Link>
+
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem
+          href="/learn"
+          label="Learn"
+          iconSrc="https://placehold.co/32x32"
+        />
+
+        <SidebarItem
+          href="/leaderboard"
+          label="Leaderboard"
+          iconSrc="https://placehold.co/32x32"
+        />
+
+        <SidebarItem
+          href="/quests"
+          label="Quests"
+          iconSrc="https://placehold.co/32x32"
+        />
+
+        <SidebarItem
+          href="/shop"
+          label="Shop"
+          iconSrc="https://placehold.co/32x32"
+        />
+      </div>
+
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton
+            // https://stackoverflow.com/a/78488757/18018704
+            appearance={{
+              elements: { userButtonPopoverCard: { pointerEvents: "initial" } },
+            }}
+          />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 }
