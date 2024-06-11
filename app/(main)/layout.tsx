8ea6119d664ae047a8471f5dac4de3832736fe3e
Nicolas <nicolasalexandre0001@gmail.com>
2024-06-11 08:42:44 -0300

    style: main page wrappers

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
       <MobileHeader />
       <Sidebar className="hidden lg:flex" />
       <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
-        <div className="h-full">{children}</div>
+        <div className="h-full mx-auto max-w-[1056px] pt-6">{children}</div>
       </main>
     </>
   );
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,3 +1,35 @@
+import FeedWrapper from "@/components/feed-wrapper";
+import StickyWrapper from "@/components/sticky-wrapper";
+import Header from "./header";
+import UserProgress from "@/components/user-progress";
+
 export default function LearnPage() {
-  return <div>página principal</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          hearts={5}
+          hasActiveSubscription={false}
+          points={200}
+          activeCourse={{
+            title: "Japanese",
+            imageSrc: "/jp.svg",
+          }}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Japanese" />
+        <div className="space-y-4">
+          <div className="w-full h-[200px] bg-blue-400" />
+          <div className="w-full h-[200px] bg-blue-400" />
+          <div className="w-full h-[200px] bg-blue-400" />
+          <div className="w-full h-[200px] bg-blue-400" />
+          <div className="w-full h-[200px] bg-blue-400" />
+          <div className="w-full h-[200px] bg-blue-400" />
+          <div className="w-full h-[200px] bg-blue-400" />
+          <div className="w-full h-[200px] bg-blue-400" />
+        </div>
+      </FeedWrapper>
+    </div>
+  );
 }
