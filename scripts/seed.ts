0f7672aeb6ed739b26a675b4b4f158ff846ded71
Nicolas <nicolasalexandre0001@gmail.com>
2024-06-20 08:45:48 -0300

    feat: update schema and seed

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -17,23 +17,86 @@ const main = async () => {
 
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
     await db.insert(schema.courses).values([
       {
+        id: 1,
         title: "Japanese",
         imageSrc: "/jp.svg",
       },
       {
+        id: 2,
         title: "Spanish",
         imageSrc: "/es.svg",
       },
       {
+        id: 3,
         title: "English",
-        imageSrc: "/usa.svg",
+        imageSrc: "/us.svg",
       },
       {
+        id: 4,
         title: "Brazilian Portuguese",
         imageSrc: "/br.svg",
+      },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 4, // Brazilian Portuguese
+        title: "Unit 1",
+        description: "Learn the basics of Brazilian Portuguese",
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1, // Unit 1 ( Learn the basics... )
+        order: 1,
+        title: "Nouns",
+      },
+      {
+        id: 2,
+        unitId: 1, // Unit 1 ( Learn the basics... )
+        order: 2,
+        title: "Verbs",
+      },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1, // Nouns
+        type: "SELECT",
+        order: 1,
+        question: 'Which one of these is the "man"',
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1, // Which one of these is the "man"
+        imageSrc: "/man.svg",
+        correct: true,
+        text: "Homem",
+        audioSrc: "/homem.mp3",
+      },
+      {
+        id: 2,
+        challengeId: 1, // Which one of these is the "man"
+        imageSrc: "/woman.svg",
+        correct: true,
+        text: "Mulher",
+        audioSrc: "/mulher.mp3",
       },
     ]);
 
