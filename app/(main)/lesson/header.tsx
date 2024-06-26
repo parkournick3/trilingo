9ee09524842fdebbc5a0a617e14e9e7d99bb1eb0
Nicolas <nicolasalexandre0001@gmail.com>
2024-06-26 08:33:52 -0300

    feat: exit lesson dialog

--- app/(main)/lesson/header.tsx
+++ app/(main)/lesson/header.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { Progress } from "@/components/ui/progress";
+import { useExitModal } from "@/store/use-exit-modal";
 import { InfinityIcon, XIcon } from "lucide-react";
 import Image from "next/image";
 
@@ -13,10 +16,12 @@ export default function Header({
   hasActiveSubscription,
   hearts,
 }: Props) {
+  const { open } = useExitModal();
+
   return (
     <header className="lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full">
       <XIcon
-        onClick={() => {}}
+        onClick={open}
         className="text-slate-500 hover:opacity-75 transition cursor-pointer"
       />
       <Progress value={percentage} />
